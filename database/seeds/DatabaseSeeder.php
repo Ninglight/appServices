04dd9883117f10ec66aede84883cf964700bb049
Ninglight <valentin.boureau@gmail.com>
2018-07-26 20:22:00 +0200

    add seeds

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,36 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        DB::table('users')->insert([
+            'name' => 'toto',
+            'email' => 'toto@connexing.com',
+            'password' => bcrypt('toto')
+        ]);
+
+        DB::table('categories')->insert([
+            'name' => 'Casques téléphoniques'
+        ]);
+
+        DB::table('brands')->insert([
+            'name' => 'Jabra',
+            'url_logo' => 'jabra.svg'
+        ]);
+
+        DB::table('products')->insert([
+            'name' => 'GN 12345',
+            'description' => 'Super casque jabra',
+            'constructor_reference' => 'AZERTY123456',
+            'connexing_reference' => 'P000001',
+            'price' => '10.56',
+            'url_ecommerce' => 'https://connexing.fr/jabra',
+            'category_id' => 1,
+            'brand_id' => 1
+        ]);
+
+        DB::table('attributes')->insert([
+            'name' => 'Jabra',
+            'url_logo' => 'jabra.svg',
+            'category_id' => 1
+        ]);
     }
 }
